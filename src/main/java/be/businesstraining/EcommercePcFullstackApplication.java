1e3c61a4e58710be84666bdad93d8471424d6f4a
Nyraen <ericvancau@gmail.com>
2019-07-18 11:43:04 +0200

    commit

---
+++ src/main/java/be/businesstraining/EcommercePcFullstackApplication.java
@@ -0,0 +1,13 @@
+package be.businesstraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePcFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercePcFullstackApplication.class, args);
+    }
+
+}
